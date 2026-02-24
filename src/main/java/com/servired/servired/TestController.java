2cc954d808be514fcb3593f99559e38c50429e75
Gabriela <gabykrav@gmail.com>
2026-02-24 18:27:27 -0300

    Corrijo endpoint

--- src/main/java/com/servired/servired/TestController.java
+++ src/main/java/com/servired/servired/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/test")
+    public String test() {
         return "Aplicación funcionando correctamente 🚀";
     }
 }
